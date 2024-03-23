7e08ef7f52e863d8a1912449bc333aebd00fd8e0
Jiří Vrba <jiri@vrba.dev>
2024-03-23 14:36:53 +0100

    Logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 
-envsubst '$PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf && rm /etc/nginx/nginx.conf.template
+echo "Replacing PORT with $PORT"
+envsubst '$PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+cat /etc/nginx/nginx.conf.template
+cat /etc/nginx/nginx.conf
+
+rm /etc/nginx/nginx.conf.template
+
+echo "Starting nginx..."
 nginx
